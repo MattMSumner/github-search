fd716f21392a0a11e23a260dcb51ae9e12671e01
Matthew Sumner <matt.m.sumner@gmail.com>
2018-06-22 11:14:11 -0400

    Load moar

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
 import { gql } from 'apollo-boost'
 import { Query } from 'react-apollo'
 
@@ -9,37 +9,85 @@ const EdgeComponent = ({ edge: { textMatches, node: { title, url } } }) => {
       <ul>
         {textMatches.map(({ fragment, highlights }, i) => {
           let startingIndice = 0
-          const strings = highlights.map(({ beginIndice, endIndice }) => {
+          const strings = highlights.map(({ beginIndice, endIndice }, j) => {
             const unHighlighted = fragment.slice(startingIndice, beginIndice);
             const highlighted = fragment.slice(beginIndice, endIndice);
             startingIndice = endIndice;
-            return (<Fragment>{unHighlighted} < mark > {highlighted}</mark></Fragment>)
+            return (<Fragment key={j}>{unHighlighted} < mark > {highlighted}</mark></Fragment>)
           })
 
-          return <li>{strings}{fragment.slice(startingIndice)}</li>
+          return <li key={i}>{strings}{fragment.slice(startingIndice)}</li>
         })}
       </ul>
     </div>
   )
 }
 
+class App extends Component {
+  state = {
+    query: null
+  }
 
-const App = () => (
-  <Query query={SEARCH} variables={{}}>
-    {({ loading, error, data }) => {
+  updateQuery = (query) => {
+    this.setState({
+      query
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBox query={this.state.query} updateQuery={this.updateQuery} />
+        {this.state.query &&
+          <ResultView query={this.state.query} />
+        }
+      </div>
+    )
+  }
+}
+
+const SearchBox = ({ query, updateQuery }) => (
+  <input
+    value={query || ''}
+    onChange={(e) => { updateQuery(e.target.value) }}
+  ></input>
+)
+
+const queryMore = (data, fetchMore) => {
+  fetchMore({
+    variables: {
+      cursor: data.search.edges.slice(-1)[0].cursor
+    },
+    updateQuery: (prev, { fetchMoreResult }) => {
+      if (!fetchMoreResult) return prev;
+      return Object.assign({}, prev, {
+        search: {
+          ...prev.search,
+          edges: [...prev.search.edges, ...fetchMoreResult.search.edges]
+        }
+      });
+    }
+  })
+}
+
+const ResultView = ({ query }) => (
+  <Query query={SEARCH} variables={{ query: query }}>
+    {({ loading, error, data, fetchMore }) => {
       if (loading) return <div>Loading...</div>
       if (error) return <div>Error :(</div>
 
       return <div>
+        <h1>Showing {data.search.edges.length}</h1>
         {data.search.edges.map(edge => <EdgeComponent edge={edge} key={edge.cursor} />)}
+        <button onClick={() => queryMore(data, fetchMore)}>Get More!</button>
       </div>
     }}
   </Query>
 )
 
 const SEARCH = gql`
-  query searchIssues {
-    search(query: "thoughtbot", type: ISSUE, first: 100) {
+  query searchIssues($query: String!, $cursor: String) {
+    search(query: $query, type: ISSUE, first: 5, after: $cursor) {
       edges {
         cursor
         textMatches {
@@ -53,6 +101,10 @@ const SEARCH = gql`
         }
         node {
           ... on Issue {
+            title
+            url
+          }
+          ... on PullRequest {
             title
             url
           }
